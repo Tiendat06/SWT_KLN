158b9c40cf6786b8dd018be1f558b9593b678d21
BoiThuy <thuyphan91ltv@gmail.com>
2025-03-01 15:45:36 +0700

    FE: fix Routes

--- FrontEnd/src/routes/aboutRoutes.js
+++ FrontEnd/src/routes/aboutRoutes.js
@@ -14,21 +14,15 @@ import {
 const aboutJRoutes = [
     // TDat Route
     {path: "/about", element: <About />},
-    {path: "/about-genealogy", element: <AboutGenealogy />},
-    {path: "/about-biography", element: <AboutBiography />},
     {path: "/about-creature", element: <AboutCreature />},
-    {path: "/about-art", element: <AboutArt />},
-    {path: "/about-books-magazines", element: <AboutBooks_Magazines/>},
-    {path: "/about-topic", element: <AboutTopic />},
-    {path: "/about-multimedia-documents", element: <AboutMultimedia_Documents />},
-
-
-
 ]
+
+
+
+
 const aboutTRoutes = [
     {path: "/about-genealogy", element: <AboutGenealogy />},
     {path: "/about-biography", element: <AboutBiography />},
-    {path: "/about-creature", element: <AboutCreature />},
     {path: "/about-books-magazines", element: <AboutBooks_Magazines/>},
     {path: "/about-topic", element: <AboutTopic />},
     {path: "/about-multimedia-documents", element: <AboutMultimedia_Documents />},
