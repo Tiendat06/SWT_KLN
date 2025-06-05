41612f4b5ea888acdc524af044e83bfb44b64d43
Tiendat06 <tadat290903@gmail.com>
2025-06-06 00:52:11 +0700

    refactor: edit enum configuration

--- FrontEnd/src/routes/B2B/loginRoutes.js
+++ FrontEnd/src/routes/B2B/loginRoutes.js
@@ -1,12 +1,13 @@
 import AppRoutesEnum from "~/enum/Route/AppRoutesEnum";
-import LoginPage from "~/pages/B2B/Login/Login"; 
+import LoginPage from "~/pages/B2B/Login/Login";
+
 const loginTRoutes = [
-    { path: `${AppRoutesEnum.AdminRoute}/login`, component: LoginPage }, 
+    {path: `${AppRoutesEnum.AdminRoute}/login`, element: <LoginPage/>},
 ];
 
 const loginRoutes = [
     ...loginTRoutes,
 ];
 
-export { loginTRoutes };
+export {loginTRoutes};
 export default loginRoutes;
