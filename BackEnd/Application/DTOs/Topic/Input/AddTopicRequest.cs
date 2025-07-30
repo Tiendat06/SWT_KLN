a6d8dbf9f67bb249ae417fd79c848b8f76a9fbaf
ManhNQ <manhnguyen61120042003@gmail.com>
2025-07-30 21:58:33 +0700

    fix C,U Topic API

--- BackEnd/Application/DTOs/Topic/Input/AddTopicRequest.cs
+++ BackEnd/Application/DTOs/Topic/Input/AddTopicRequest.cs
@@ -18,12 +18,14 @@ namespace Application
 
     public class AddTopicImageRequest
     {
+        public int Id { get; set; }
         public string? Capture { get; set; }
         public IFormFile ImageLink { get; set; }
     }
 
     public class AddTopicVideoRequest
     {
+        public int Id { get; set; }
         public string? Capture { get; set; }
         public IFormFile VideoLink { get; set; }
     }
